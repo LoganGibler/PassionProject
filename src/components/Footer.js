fb74b4fdc7890e41174052e0163bce8db7315c31
Logan Gibler <Logan.Gibler@gmail.com>
2022-04-02 01:08:48 -0400

    quick refresher

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <header>
+      <h1>Welcome to the Footer Component</h1>
+    </header>
+  );
+};
+
+export default Footer;
